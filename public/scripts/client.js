5a2656ad9cb4596583e8d180386e6c8f0eb06a28
Zea <78378462+zeamaria@users.noreply.github.com>
2021-04-24 18:29:08 -0700

    Rendering tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
  const data = [
   {
     "user": {
@@ -28,41 +27,36 @@
     },
     "created_at": 1461113959088
   }
-]
+];
 
 // replace @username with actual data / date/ timeago = created_at time
-
-
 const createTweetElement = function(tweetData) {
   let newTweet = `
   <article class="user-tweet">
-  <header class="user-tweet-header">
-    <h5>User</h5>
-    <h4>@username</h4>
-  </header>
-  <div class="user-tweet-body">
-    <p>simply dummy text of the printing and typesetting industry.</p>
-  </div>
-  <footer class="user-tweet-footer">
-    <h6>
-      <time class="timeago" datetime="2020-07-17T09:24:17Z">July 17, 2020</time>
-    </h6>
-    <div class="icons">
-      <i class="fas fa-flag icon"  w3-tiny></i>
-      <i class="fas fa-retweet icon" w3-tiny></i>
-      <i class="fas fa-heart icon" w3-tiny></i>
+    <header class="user-tweet-header">
+      <h5>User</h5>
+      <h4>${tweetData.user.handle}</h4>
+    </header>
+    <div class="user-tweet-body">
+      <p>${tweetData.content.text}</p>
     </div>
-  </footer>
-</article> `
+    <footer class="user-tweet-footer">
+      <h6>
+        <time class="timeago" datetime="${tweetData.created_at}">Now</time>
+      </h6>
+      <div class="icons">
+        <i class="fas fa-flag icon" w3-tiny></i>
+        <i class="fas fa-retweet icon" w3-tiny></i>
+        <i class="fas fa-heart icon" w3-tiny></i>
+      </div>
+    </footer>
+  </article>`;
 
-return newTweet;
-
+  return newTweet;
 };
 
-const renderTweets = function(tweets) {
-  tweets.forEach((tweet) => {
-    console.log(tweet)
+const renderTweets = function() {
+  data.forEach((tweetData) => {
+    $( "#tweet-container" ).append( createTweetElement(tweetData) );
   })
 }
-
-renderTweets(data);
