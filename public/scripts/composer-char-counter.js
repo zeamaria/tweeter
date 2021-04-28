5e293dcd4bfdd57cea107d4a3e77df3718457d24
Zea <78378462+zeamaria@users.noreply.github.com>
2021-04-27 21:35:33 -0700

    Add compose character color change css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
   $("#tweet-text").on("input", function(){
     let value = 140 - $(this).val().length
     $(".counter").text(value);
-    $(".counter").css("color", "red");
-    // if less than 140 go red when go < 0  less than. 
+    if (value < 0) {
+      $(".counter").css("color", "red");
+    }
+    else {
+      $(".counter").css("color", "black");
+    }
   })
 });
 
